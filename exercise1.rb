e6aace6e3ec19865e0ee3a3387db66338cff1472
Arash <arash.ouji@safe.com>
2014-01-21 09:44:13 -0800

    Now complete greater than 10s

--- exercise1.rb
+++ exercise1.rb
@@ -1,1 +1,12 @@
-exercise1.rb
+def greater_than_tens (input)
+	output=Array.new
+	input.each do |inp|
+		if inp.kind_of?(Numeric)  && inp > 10
+			output << inp
+		end
+	end
+	return output
+end
+
+a=['fefee',2,14, 14.4, 500, 0.4]
+p greater_than_tens a
