1fa0eb7064d13d2f6883572a555e0962a32e2704
Arash <arash.ouji@safe.com>
2014-01-21 09:53:18 -0800

    Greater than 10s in one line

--- exercise1.rb
+++ exercise1.rb
@@ -1,11 +1,5 @@
 def greater_than_tens (input)
-	output=Array.new
-	input.each do |inp|
-		if inp.kind_of?(Numeric)  && inp > 10
-			output << inp
-		end
-	end
-	return output
+	input.delete_if {|inp| !(inp.kind_of?(Numeric)) || inp <= 10}
 end
 
 a=['fefee',2,14, 14.4, 500, 0.4]
