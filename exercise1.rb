a64ebd59a252b4a87989f1b2ef5643078f318f88
Arash <arash.ouji@safe.com>
2014-01-21 10:21:37 -0800

    Implement greater than 10s non-descructive

--- exercise1.rb
+++ exercise1.rb
@@ -1,5 +1,6 @@
 def greater_than_tens (input)
-	input.delete_if {|inp| !(inp.kind_of?(Numeric)) || inp <= 10}
+	# input.delete_if {|inp| !(inp.kind_of?(Numeric)) || inp <= 10}
+	input.select{|inp| inp.kind_of?(Numeric) && inp > 10}
 end
 
 a=['fefee',2,14, 14.4, 500, 0.4]
